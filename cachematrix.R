bef9e16da9eb7e28c32e8fb09ec16309d8348c67
claudio <claudio.lande@yahoo.it>
2019-07-03 09:42:19 +0200

    first implementation. solve fails because it does not get a matrix but a vector

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedInverseMatrix <- NULL
+    
+    # Retrieves the original matrix
+    getData <- function() x
+    
+    # Retrieves the inverse matrix
+    getInverseMatrix <- function() cachedInverseMatrix
+    
+    # Sets the inverse matrix
+    setInverseMatrix <- function(inversedMatrix) {
+        cachedInverseMatrix <<- inversedMatrix
+    }
+    
+    list(getData=getData, 
+         getInverseMatrix=getInverseMatrix, 
+         setInverseMatrix=setInverseMatrix)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrixToInvert, ...) {
+    inverseMatrix <- matrixToInvert$getInverseMatri()
+    if(!is.null(inverseMatrix)) {
+        message("Returning cached inverse matrix")
+        return(inverseMatrix)
+    }
+    # if there is no cached inverse matrix, calculate and store it
+    matrixData <- matrixToInvert$getData()
+    inverseMatrix <- solve(matrixData, ...)
+    matrixToInvert$setInverseMatrix(inverseMatrix)
+    inverseMatrix
 }
